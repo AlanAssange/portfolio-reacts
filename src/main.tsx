91b6c7bac7d0ec4e60a12df873fbf1a1ae47b585
AlanAssange <alanjc27@gmail.com>
2025-01-29 23:25:41 -0300

    changes for index.title and imports erased

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import '../index.css';
 import App from './App.tsx';
 import { Provider } from './components/ui/provider.tsx';
 import IntroLayer from './components/IntroLayer.tsx';
